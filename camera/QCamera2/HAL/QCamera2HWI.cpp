61e5d591a0aa1d291cc9f131a544d60adfeb57c8
Sauhard Pande <sauhardp@codeaurora.org>
2017-09-27 11:54:49 +0530

    QCamera2:HAL :Changes to get the vsync event and presentation timestamp
    Issue: Direct call to libgui to get event caused issue of
    vendor to sytem access.
    
    Fix: Access to event using IDisplayservice, to create presentation timestamp
    to avoid preview jitter.
    
    Change-Id: I4d16d86f892853d8bd048e4d9d98925555d30f68
    (cherry-picked from commit 38f90d257451622e53c84b191d9a49b7fa062865
    from platform/hardware/qcom/camera for msm-4.9; replaced android.hidl.base@1.0 with libhidltranspot)
    
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -46,6 +46,7 @@
 #include "QCameraBufferMaps.h"
 #include "QCameraFlash.h"
 #include "QCameraTrace.h"
+#include "QCameraDisplay.h"
 
 extern "C" {
 #include "mm_camera_dbg.h"
@@ -1678,6 +1679,13 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(uint32_t cameraId)
     mCameraDevice.ops = &mCameraOps;
     mCameraDevice.priv = this;
 
+    mCameraDisplay = new QCameraDisplay();
+
+    pthread_condattr_t mCondAttr;
+
+    pthread_condattr_init(&mCondAttr);
+    pthread_condattr_setclock(&mCondAttr, CLOCK_MONOTONIC);
+
     pthread_mutex_init(&m_lock, NULL);
     pthread_cond_init(&m_cond, NULL);
 
@@ -1705,6 +1713,12 @@ QCamera2HardwareInterface::QCamera2HardwareInterface(uint32_t cameraId)
     mDeferredWorkThread.launch(deferredWorkRoutine, this);
     mDeferredWorkThread.sendCmd(CAMERA_CMD_TYPE_START_DATA_PROC, FALSE, FALSE);
     m_perfLock.lock_init();
+
+#ifdef USE_DISPLAY_SERVICE
+    DeferWorkArgs args;
+    memset(&args, 0, sizeof(args));
+    queueDeferredWork(CMD_DEF_DISPLAY_INIT, args);
+#endif //USE_DISPLAY_SERVICE
 
     pthread_mutex_init(&mGrallocLock, NULL);
     mEnqueuedBuffers = 0;
@@ -1753,6 +1767,11 @@ QCamera2HardwareInterface::~QCamera2HardwareInterface()
     closeCamera();
     m_perfLock.lock_rel();
     m_perfLock.lock_deinit();
+
+    if (mCameraDisplay != NULL) {
+         delete mCameraDisplay;
+         mCameraDisplay = NULL;
+    }
     pthread_mutex_destroy(&m_lock);
     pthread_cond_destroy(&m_cond);
     pthread_mutex_destroy(&m_evtLock);
@@ -1969,6 +1988,13 @@ int QCamera2HardwareInterface::openCamera()
         mBootToMonoTimestampOffset = getBootToMonoTimeOffset();
     }
     LOGH("mBootToMonoTimestampOffset = %lld", mBootToMonoTimestampOffset);
+
+#ifdef USE_DISPLAY_SERVICE
+         if(!mCameraDisplay->startVsync(TRUE))
+         {
+             LOGE("Error: Cannot start vsync (still continue)");
+         }
+#endif //USE_DISPLAY_SERVICE
 
     return NO_ERROR;
 
@@ -2249,6 +2275,10 @@ int QCamera2HardwareInterface::closeCamera()
         LOGD("Failed to release flash for camera id: %d",
                 mCameraId);
     }
+
+#ifdef USE_DISPLAY_SERVICE
+        mCameraDisplay->startVsync(FALSE);
+#endif //Use_DISPLAY_SERVICE
 
     LOGI("[KPI Perf]: X PROFILE_CLOSE_CAMERA camera id %d, rc: %d",
          mCameraId, rc);
@@ -9579,6 +9609,13 @@ void *QCamera2HardwareInterface::deferredWorkRoutine(void *obj)
                         job_status = bgTask->bgFunction(bgTask->bgArgs);
                     }
                     break;
+#ifdef USE_DISPLAY_SERVICE
+                case CMD_DEF_DISPLAY_INIT:
+                    {
+                        pme->mCameraDisplay->init();
+                    }
+                    break;
+#endif //USE_DISPLAY_SERVICE
                 default:
                     LOGE("Incorrect command : %d", dw->cmd);
                 }
