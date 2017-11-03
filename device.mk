6bfe0c900238e1e0c121df909478f54b1de8267a
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-11-03 04:54:43 -0600

    land: add thermal HALS
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -346,6 +346,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
+#Thermal
+PRODUCT_PACKAGES += \
+   android.hardware.thermal@1.0-impl \
+   android.hardware.thermal@1.0-service
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
