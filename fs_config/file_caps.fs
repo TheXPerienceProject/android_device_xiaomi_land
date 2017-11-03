7b79e713904238e218b1c2d7ec4af6330825fe41
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-11-03 05:03:44 -0600

    land: update file_caps
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- fs_config/file_caps.fs
+++ fs_config/file_caps.fs
@@ -21,7 +21,19 @@ group: AID_SYSTEM
 mode: 0755
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsdatadaemon]
+user: AID_SYSTEM
+group: AID_SYSTEM
+mode: 0755
+caps: NET_BIND_SERVICE
+
 [system/vendor/bin/ims_rtp_daemon]
+user: AID_SYSTEM
+group: AID_RADIO
+mode: 0755
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
 user: AID_SYSTEM
 group: AID_RADIO
 mode: 0755
