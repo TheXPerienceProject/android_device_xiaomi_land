ba2ef307f4cedbfcf4534fd50d20d5b767253f7e
klozz <klozz707@gmail.com>
2017-11-20 03:55:06 -0500

    land: add more drm related
    Change-Id: I7da090aa244c5b2386213294f4c60668409ae5e1
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -168,7 +168,9 @@ PRODUCT_PACKAGES += \
 
 # DRM HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine
 
 # Fingerprint
 PRODUCT_PACKAGES += \
