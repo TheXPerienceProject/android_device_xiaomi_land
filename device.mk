e9b8e82cd95997b7f940972b563d93f371c29565
klozz <klozz707@gmail.com>
2017-11-26 02:11:14 -0500

    land: fix this it can be added as prebuilt file we can't build it from source my bad xd
    Change-Id: I329d0182b93110945d5f2acdd2b79a39881a856a
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -169,8 +169,7 @@ PRODUCT_PACKAGES += \
 # DRM HIDL
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \
