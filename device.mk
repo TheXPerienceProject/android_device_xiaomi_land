32954ba34f43d04172b72cb6486662c75e1dc5ae
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 08:15:10 -0500

    land:Build a dummy android.hidl.base@1.0 for Oreo-MR0 blobs
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -114,9 +114,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# TEMP FIX
+# Dummy hidl + hidl manager
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+	android.hidl.manager@1.0-java
 
 # Bluetooth HIDL
 PRODUCT_PACKAGES += \
