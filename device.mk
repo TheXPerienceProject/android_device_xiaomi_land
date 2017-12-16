030379ea11c911ab177dd674b4d17edf49995bb7
klozz <klozz707@gmail.com>
2017-12-15 21:18:02 -0500

    [TMPFIX]:land:build android.hidl.base@1.0
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -111,6 +111,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# TEMP FIX
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0
+
 # Bluetooth HIDL
 PRODUCT_PACKAGES += \
     libbt-vendor \
