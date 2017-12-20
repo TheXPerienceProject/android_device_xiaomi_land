cbf8912178dae3f3b12549d5eec1bda48f762651
klozz <klozz707@gmail.com>
2017-12-19 21:30:31 -0500

    land: get rid of System mount from fstab it can be handle with dts also add fstab for the recovery it need to add system partition
    Signed-off-by: klozz <klozz707@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -221,7 +221,7 @@ TARGET_POWERHAL_VARIANT := qcom
 QCOM_DISPLAY_VARIANT := msm8937
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 #TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
 TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
