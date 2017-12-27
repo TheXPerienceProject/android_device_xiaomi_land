08afbd4fbf6b2feaddf6356fa9c4e30b10af656c
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-27 23:39:54 +0200

    land: Add VNDK HALs
    Change-Id: I1723d5531674b1d84c711a655f6476544bb0a956
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -406,6 +406,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
