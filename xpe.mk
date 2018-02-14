5518c35413c6d715ee437391ba0d3a1e3f6bb8ee
klozz <klozz707@gmail.com>
2018-02-14 00:07:00 -0500

    land: Set shipping API level via inherited makefile
    Signed-off-by: klozz <klozz707@gmail.com>

--- xpe.mk
+++ xpe.mk
@@ -19,6 +19,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/xperience/config/common_full_phone.mk)
 
