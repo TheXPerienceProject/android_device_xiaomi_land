cf26887c1ae22191493c0ef4a8db743c5a523ea1
klozz <klozz707@gmail.com>
2018-02-16 03:39:13 -0500

    land: binderize more things
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -87,9 +87,13 @@ PRODUCT_PACKAGES += \
 #Audio HIDL
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+	android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+	android.hardware.audio.effect@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+	android.hardware.broadcastradio@1.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+	android.hardware.soundtrigger@2.0-service \
 
 #Audio mixer
 PRODUCT_COPY_FILES += \
@@ -179,6 +183,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.memtrack@1.0-impl \
+	android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl
 
 # Display calibration
