89faf49cc0f2b131cf53154342233fc0764eae36
klozz <klozz707@gmail.com>
2018-03-05 18:43:41 -0500

    land: build hidl.base@1.0_vendor
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -123,6 +123,7 @@ PRODUCT_COPY_FILES += \
 # Dummy hidl + hidl manager
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+	android.hidl.base@1.0_vendor \
     android.hidl.manager@1.0 \
     android.hidl.manager-V1.0-java
 
