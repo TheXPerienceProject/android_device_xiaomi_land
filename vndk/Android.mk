553460b330d8955f55053e92fa139f4cac6fd6fd
klozz <shaclone707@gmail.com>
2018-08-18 07:32:13 -0400

    land:move to Versioned VNDK
    Signed-off-by: klozz <shaclone707@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -48,6 +48,8 @@ EXTRA_VENDOR_LIBRARIES := \
 #-------------------------------------------------------------------------------
 LOCAL_PATH := $(call my-dir)
 
+vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
+
 define define-vndk-lib
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.$2
@@ -59,7 +61,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $3
-LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -74,18 +75,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_RELATIVE_PATH := $3
-LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
 endef
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,vndk-sp,)))
+    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,$(vndk_sp_dir),)))
 $(foreach lib,$(VNDK_SP_EXT_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-sp-ext-gen,vndk-sp,true)))
+    $(eval $(call define-vndk-lib,$(lib),vndk-sp-ext-gen,$(vndk_sp_dir),true)))
 $(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,,true)))
+    $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,xpe,true)))
 
 
 #-------------------------------------------------------------------------------
