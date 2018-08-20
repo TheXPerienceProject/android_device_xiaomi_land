a8d2ea708b9ec0d7ba1a7c1b90c672c5cc446303
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-08-20 13:11:25 +0530

    land: : audio: add new vendor specific media codec file Add new vendor specific media codec file which contains vendor specific codec entries
    Change-Id: I6b22674e395d7b73b7e54d107bf9df19404a9c9c
    CRs-Fixed: 2289857
    
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -332,6 +332,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
