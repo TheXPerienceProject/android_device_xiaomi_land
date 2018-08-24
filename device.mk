a6e125905aaf4692871ec4ad1eededcceeb730cf
klozz <shaclone707@gmail.com>
2018-08-24 02:04:55 -0400

    land: move to  android.hardware.usb@1.0-service.basic    added in our hardware interface  https://github.com/TheXPerienceProject/android_hardware_xperience_interfaces/commit/20324e298429802def6fced88771d2dc0fb2e670
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -427,7 +427,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 #vibrator HIDL HAL
 PRODUCT_PACKAGES += \
