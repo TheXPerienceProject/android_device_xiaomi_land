7ac24ed57c5afe918d3996d87d176eb770df2daf
klozz <shaclone707@gmail.com>
2018-09-12 09:38:54 -0400

    land: Add audio HAL 4 (TREBLE compliance)
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -95,7 +95,13 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-service\
+		android.hardware.audio@4.0 \
+    android.hardware.audio.common@4.0 \
+    android.hardware.audio.common@4.0-util \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0 \
+    android.hardware.audio.effect@4.0-impl
 
 #Audio mixer
 PRODUCT_COPY_FILES += \
