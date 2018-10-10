fc405a7783945e815851f9bed4b9d97256147c79
Nich <nctrenco@gmail.com>
2018-10-11 07:23:12 +0800

    land: camera: Account for HFR usecase
    Change-Id: I073264683fd2e1f4065557ce09ed569dd1e1113f
    Signed-off-by: klozz <xXx.Reptar.Rawrr.xXx@gmail.com>

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -6457,6 +6457,13 @@ int32_t QCameraParameters::setPreviewFpsRange(int min_fps,
     property_get("persist.debug.set.fixedfps", value, "0");
     fixedFpsValue = atoi(value);
 
+    // Don't allow function callers to request min fps same as max fps
+    // I mean SnapdragonCamera.
+    if (!isHfrMode() && max_fps >= 24000 && min_fps == max_fps) {
+        LOGH("min_fps %d same as max_fps %d, setting min_fps to 7000", min_fps, max_fps);
+        min_fps = 7000;
+    }
+
     LOGD("E minFps = %d, maxFps = %d , vid minFps = %d, vid maxFps = %d",
                  min_fps, max_fps, vid_min_fps, vid_max_fps);
 
