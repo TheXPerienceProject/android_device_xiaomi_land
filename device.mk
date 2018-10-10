3fa7923e034c96a9cb56c4b4c715035f67933c76
klozz <shaclone707@gmail.com>
2018-10-10 09:26:42 -0400

    land: Use Health 2.0
    Signed-off-by: klozz <shaclone707@gmail.com>

--- device.mk
+++ device.mk
@@ -253,6 +253,7 @@ PRODUCT_COPY_FILES += \
 #Healthd hal
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
+    android.hardware.health@2.0-impl \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-service
 
