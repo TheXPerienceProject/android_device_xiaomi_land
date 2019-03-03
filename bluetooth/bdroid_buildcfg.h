c336d8d8f597130725bc5592ad0e59fd6d1f58df
klozz <shaclone707@gmail.com>
2019-03-03 04:35:31 -0500

    land: set undef PROPERTY_VALUE_MAX to fix bt build
    Change-Id: I0c93e97cac827545b81d65c6130975c0093baa4d
    Signed-off-by: klozz <shaclone707@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -49,4 +49,7 @@ inline const char* BtmGetDefaultName()
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BTM_WBS_INCLUDED TRUE
 #define BTIF_HF_WBS_PREFERRED TRUE
+
+#undef PROPERTY_VALUE_MAX
+
 #endif
